be877e1fa356f94ac10c93dba2d3f03b1680f081
brenten lovato <brentengust@gmail.com>
2016-01-30 12:44:02 -0800

    update latlngs after form

--- client/templates/Riotr.js
+++ client/templates/Riotr.js
@@ -76,7 +76,7 @@ Template.Riotr.rendered = function() {
     $('#new_update_dialog').on('hidden.bs.modal', function () {
         // create point centered on map
         var divIcon = L.divIcon({className: 'div-icon'});
-        var marker = L.marker([0,0], {icon: divIcon}).addTo(map);
+        var marker = L.marker(map.getCenter(), {icon: divIcon}).addTo(map);
 
         map.on('move', function () {
             marker.setLatLng(map.getCenter());
@@ -91,7 +91,9 @@ Template.Riotr.rendered = function() {
             lng = Number(position['lng']);
 
             console.log(position);
-            //setLeafLatLong(lat, lng);
+            EventUpdates.update(Session.get("newestUpdateID"), {
+                $set: {lat: lat, lng: lng, created_on: new Date()}
+            });
 
         });
     });
