cdf36d8149d7fe93ce0117f719c68d281de246e1
Joseph Faulkner <jofaulkn@ucsc.edu>
2016-01-29 22:13:22 -0800

    Map adds clicks to collection

--- client/templates/Riotr.js
+++ client/templates/Riotr.js
@@ -12,9 +12,48 @@ Template.hello.events({
     Session.set('counter', Session.get('counter') + 1);
   }
 });
-
+/*
 Template.update_list.helpers({
   get_updates: function () {
   return event_updates.find();
 }
 });
+*/
+
+Meteor.startup(function() {
+   $(window).resize(function(){
+      $('#map').css('height', window.innerHeight);
+   });
+   $(window).resize();
+});
+
+//temporary test markers
+//var Markers = new Mongo.Collection('markers');
+//Meteor.subscribe('markers');
+Template.Riotr.rendered = function() {
+
+
+   var map = L.map('map', {
+      doubleClickZoom: false
+   }).setView([36.9719, -122.0264], 13);
+
+   L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
+   //Markers.insert({lat: 5, lng: 5});
+
+   map.on('dblclick', function(event) {
+      EventUpdates.insert({lat: event.latlng.lat, lng: event.latlng.lng});
+      console.log (event.latlng.lat, event.latlng.lng);
+   });
+
+   var query = EventUpdates.find();
+   query.observe({
+      added: function (document) {
+         var marker = L.marker(document.latlng).addTo(map)
+         .on('click', function(event) {
+            map.removeLayer(marker);
+            EventUpdates.remove({_id: document._id});
+         });
+      }
+   });
+
+}
