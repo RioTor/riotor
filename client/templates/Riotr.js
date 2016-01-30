67bd401cf3390eccd6ef9e482148ef000b96550f
galarus <dwmarx@ucsc.edu>
2016-01-29 21:46:32 -0800

    this is the first map test

--- client/templates/Riotr.js
+++ client/templates/Riotr.js
@@ -1,14 +1,37 @@
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
+/*Meteor.startup(function() {
+   $(window).resize(function(){
+      $('#map').css('height', window.innerHeight - 100);  
+   });
+   $(window).resize();
 });
+*/
+//temporary test markers
+var Markers = new Mongo.Collection('markers');
+Meteor.subscribe('markers');
+Template.Riotr.rendered = function() {
 
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
-});
+   
+   var map = L.map('map', {
+      doubleClickZoom: false
+   }).setView([36.9719, -122.0264], 13);
+   
+   L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
+   Markers.insert({lat: 5, lng: 5});
+
+   map.on('dblclick', function(event) {
+      Markers.insert({lat: event.latlng.lat, lng: event.latlng.lng});
+      console.log (event.latlng.lat, event.latlng.lng);
+   });
+   
+   var query = Markers.find();
+   query.observe({
+      added: function (document) {
+         var marker = L.marker(document.latlng).addTo(map)
+         .on('click', function(event) {
+            map.removeLayer(marker);
+            Markers.remove({_id: document._id});
+         });
+      }
+   });
+   
+}
