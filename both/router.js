9309b45b5523c64b1533fcbce0915c6a8a80c93a
brenten lovato <brentengust@gmail.com>
2016-01-30 16:52:01 -0800

    /event datacontext

--- both/router.js
+++ both/router.js
@@ -5,10 +5,11 @@ Router.route('/', function () {
 });
 Router.route('/events', function () {
   this.render('events', {
-    data: function () { /*return Items.findOne({_id: this.params._id});*/ }
+    data: function () { return Events.find( { "isPrivate": { $in: [ true ] } } 
+   }); 
   });
 });
-Router.route('map', function () {
+Router.route('/map', function () {
   this.render('Riotr', {
     data: function () { /*return Items.findOne({_id: this.params._id});*/ }
   });
