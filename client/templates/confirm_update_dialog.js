0e12f7c8d4adfdf670afbbbb915a9d3f6ae019a8
Joseph Faulkner <jofaulkn@ucsc.edu>
2016-01-30 16:40:29 -0800

    Users can now add events easily.

---
+++ client/templates/confirm_update_dialog.js
@@ -0,0 +1,32 @@
+
+
+    Template.confirm_update_dialog.events({
+      'click #confirm_submit': function(){
+        var new_data = Session.get('latest_update');
+        var tag = new_data.tag;
+        var text = new_data.text;
+        var isPrivate = new_data.isPrivate;
+        var userName = new_data.userName;
+        var uid = new_data.uid;
+
+        console.log(" ",tag," ", text, " ", isPrivate, " ", userName, " ", uid);
+
+          var position = Session.get('latest_position');
+          var lat = position.lat;
+          var lng = position.lng;
+
+         EventUpdates.insert({
+                 lat: lat,
+                 lng: lng,
+                 tag: tag,
+                 text: text,
+                 isPrivate: isPrivate,
+                 name: userName,
+                 uid: uid
+             }, function(error, result){
+               Session.set("newestUpdateID", result);
+             });
+        $('#confirm_update_dialog').css('display', 'none');
+
+      }
+    });
--- client/templates/new_update_dialog.js
+++ client/templates/new_update_dialog.js
@@ -1,3 +1,11 @@
+Meteor.startup(function() {
+
+    $('#confirm_update_dialog').css('display', 'none');
+
+    //$(window).resize();
+});
+
+
 Template.new_update_dialog.events({
   'click #confirmText': function(){
 
@@ -23,6 +31,9 @@ Template.new_update_dialog.events({
     name: userName,
     uid: uid
     });
+
+    console.log(Session.get('latest_update'));
+    /*
     EventUpdates.insert({
             tag: tag,
             text: text,
@@ -32,7 +43,9 @@ Template.new_update_dialog.events({
         }, function(error, result){
           Session.set("newestUpdateID", result);
         });
-
+     */
     $('#new_update_dialog').modal("hide");
+    $('#confirm_update_dialog').css('display', 'block');
+
   }
 });
