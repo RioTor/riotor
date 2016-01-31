8b65ec7b1b7d0a26915324bce858ad9edd6bac1c
brenten lovato <brentengust@gmail.com>
2016-01-30 16:59:15 -0800

    syntaax

--- both/router.js
+++ both/router.js
@@ -6,9 +6,9 @@ Router.route('/', function () {
 Router.route('/events', function () {
   this.render('events', {
     data: function () { /*return Events.find( { "isPrivate": { $in: [ false ] } */ } 
-   }); 
-  });
+  }); 
 });
+
 Router.route('/map', function () {
   this.render('Riotr', {
     data: function () { /*return Items.findOne({_id: this.params._id});*/ }
